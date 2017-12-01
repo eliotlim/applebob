39284c69d0a7cd2203c266052856bb59ee803e1f
missingchromosomes <noemaill@gmail.com>
2017-12-01 20:48:29 +0100

    added player1&2 scoreboard

--- src/space/sufficient/applebob/render/HPCRenderTarget.java
+++ src/space/sufficient/applebob/render/HPCRenderTarget.java
@@ -5,6 +5,7 @@ import space.sufficient.applebob.world.Tile;
 import space.sufficient.applebob.world.World;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import java.awt.*;
@@ -16,16 +17,18 @@ public class HPCRenderTarget extends RenderTarget {
     private long mFpsTime;
     JTextArea drawTextArea;
     JSlider slider;
+    JLabel score1, score2;
 
     public HPCRenderTarget(int width, int height) {
         this.mViewportWidth = width;
         this.mViewportHeight = height;
         JFrame f = new JFrame("HPC");
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        f.setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
 
         JPanel cPanel = new JPanel();
-        cPanel.setLayout(new BoxLayout(cPanel, BoxLayout.PAGE_AXIS));
-        f.getContentPane().add(cPanel);
+        cPanel.setLayout(new BoxLayout(cPanel, BoxLayout.Y_AXIS));
+        f.getContentPane().add(cPanel, BorderLayout.PAGE_START);
 
         drawTextArea = new JTextArea("",height,width);
         drawTextArea.setFont(new Font("monospaced", Font.PLAIN, 12));
@@ -34,12 +37,44 @@ public class HPCRenderTarget extends RenderTarget {
         drawTextArea.setEditable(false);
 
         cPanel.add(drawTextArea);
+
+
+        JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(new GridLayout(1,2));
+        f.getContentPane().add(scorePanel,BorderLayout.CENTER);
+        for (int i =1; i<3; i++){
+            JPanel tPanel = new JPanel();
+            tPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+            tPanel.setLayout(new BoxLayout(tPanel,BoxLayout.PAGE_AXIS));
+
+            JLabel playerLabel = new JLabel("Player "+i, JLabel.CENTER);
+            playerLabel.setPreferredSize(new Dimension(150,70));
+            playerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            tPanel.add(playerLabel, Component.CENTER_ALIGNMENT);
+            scorePanel.add(tPanel);
+
+            JLabel scoreLabel = new JLabel("0", JLabel.CENTER);
+            scoreLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            scoreLabel.setPreferredSize(new Dimension(150,70));
+            tPanel.add(scoreLabel, Component.CENTER_ALIGNMENT);
+
+            if (i==1) {
+                score1 = scoreLabel;
+            } else {
+                score2 = scoreLabel;
+            }
+        }
+
+        JPanel miscPanel = new JPanel();
+        miscPanel.setLayout(new BoxLayout(miscPanel, BoxLayout.PAGE_AXIS));
+        f.getContentPane().add(miscPanel,BorderLayout.PAGE_END);
+
         JLabel sliderLabel = new JLabel("Retro --->", JLabel.CENTER);
         sliderLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        cPanel.add(sliderLabel);
+        miscPanel.add(sliderLabel);
         slider = new JSlider(0,155,0);
         slider.addChangeListener(e -> sliderChanged());
-        cPanel.add(slider);
+        miscPanel.add(slider);
 
         f.pack();
         f.setVisible(true);
